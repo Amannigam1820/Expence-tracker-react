e1d2e929ef113868c04fdb4050240ab0658c4a38
Amannigam1820 <amannigam18082001@gmail.com>
2025-01-06 17:18:14 +0530

    checking update while pushed code

--- src/component/TitleAndBalance.jsx
+++ src/component/TitleAndBalance.jsx
@@ -8,7 +8,7 @@ const TitleAndBalance = () => {
   return (
     <div>
         <div className="">
-        <h3 className="mt-8 ml-4 text-2xl  font-bold float-left">Expence Tracker</h3>
+        <h3 className="mt-8 ml-4 text-2xl  font-bold float-left">Expence Tracker deploy CICD</h3>
          <div className="float-right mr-5 mt-10">
           <span>Your balance update: <b>${expence.balance}</b> </span>
           
